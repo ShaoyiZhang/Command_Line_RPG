83441e9fce07c0de100d91ebb5caeee8aad60ab9
Xujia Cao <xujiacao@gmail.com>
2015-12-19 15:28:06 -0800

    add description

--- Item.h
+++ Item.h
@@ -11,6 +11,7 @@ class Item
 {
 private:
     string name;
+	string description;
 	int buyInPrice;
 	int sellOutPrice;
 	// each int indicate an attribute to be changed after using this item
@@ -18,12 +19,14 @@ private:
 	vector<int> modifyAttributes;
 
 public:
-	Item(string name, int buyInPrice, int sellOutPrice):name(name),buyInPrice(buyInPrice), sellOutPrice(sellOutPrice){};
-	Item(string name, int buyInPrice, int sellOutPrice, vector<int> modifyAttributes){};
+	Item(string name,string description, int buyInPrice, int sellOutPrice):name(name),buyInPrice(buyInPrice), sellOutPrice(sellOutPrice){};
+	Item(string name, string description, int buyInPrice, int sellOutPrice, vector<int> modifyAttributes){};
 	void use();
     bool operator < (const Item &right) const;
     int GetBuyPrice(){ return this->buyInPrice; };
     int GetSellPrice(){ return this->sellOutPrice; };
+	string GetName() { return name; }
+	string GetDescription() { return description; }
 };
 
 #endif
