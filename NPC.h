c8b226e68af306da74b54e511399ca0f0c16cbab
Xujia Cao <xujiacao@gmail.com>
2015-12-19 15:57:42 -0800

    NPC

--- NPC.h
+++ NPC.h
@@ -1,7 +1,31 @@
 #ifndef NPC_H_
 #define NPC_H_
+#include "Hero.h"
+#include<iostream>
+#include <time.h>
 
-#include <iostream>
+using std::pair;
+using std::cout;
+using std::cin;
+using std::endl;
+
+class NPC {
+private:
+	vector<Item> itemForSale;
+	vector<Item> specialItem;
+	vector<string> talkings;
+	string specialMessage;
+public:
+	NPC(vector<Item> itemForSale, vector<Item> specialItem, vector<string>talkings, string specialMessage);
+	void Talking(Hero& h);
+	void Trade(Hero & h);
+	void TradeSpecialItem(Hero & h);
+	
+	//improve the quality of the equiment, might result in the destroy of the equiment 
+	//TO DO
+	//void Enhance(Hero & h);
+
+};
 
 
 #endif
