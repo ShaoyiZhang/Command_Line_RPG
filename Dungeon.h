5976dbe6194f8e841f11f3fdb4de471ce0620ac7
Xujia Cao <xujiacao@gmail.com>
2015-12-18 22:02:02 -0800

    fix constructor

---
+++ Dungeon.h
@@ -0,0 +1,11 @@
+#ifndef DUNGEON_H_
+#define DUNGEON_H_
+#include "Monster.h"
+
+class Dungeon() {
+	vector<vector<Monster>>stages;
+}
+
+
+
+#endif
--- Monster.h
+++ Monster.h
@@ -1,3 +1,6 @@
+#ifndef MONSTER_H_
+#define MONSTER_H_
+
 #include "Life.h"
 #include "Item.h"
 #include <time.h>
@@ -10,9 +13,15 @@ private:
 	int gold;
 	int EXP;
 public:
+	//Life(string name, int attack, int defense, int stamina, int intelligence,
+		//int stun, int weak, int HP, int MP, int totalHP, int totalMP, vector<int> skills);
+	Monster(string name, int attack, int defense, int stamina, int intelligence,
+		int TotalHP, int TotalMP, vector<int>skills, vector<std::pair<Item, int>>dpitems, int g, int exp)
+		: Life(name, attack, defense, stamina, intelligence, 0, 0, TotalHP, TotalMP, totalHP, TotalMP, skills), dropItems(dpitems), gold(g), EXP(exp) {}
 	int GiveGold();
 	int GiveEXP();
 	vector<Item> DropOff();
 };
 
+#endif
 
