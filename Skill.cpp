d964af0c76a00c344a1d4c22d7f03931daeb1fc3
Shaoyi Zhang <zhangshaoyi1995@gmail.com>
2015-12-19 17:02:45 -0800

    update Skill class

--- Skill.cpp
+++ Skill.cpp
@@ -1,9 +1,37 @@
 #include "Skill.h"
 
-Skill::Skill()
-{
+Skill::SingleSkill::SingleSkill(vector<pair<int,int>> modifySelfAttributes, vector<pair<int,int>> modifyMonsterAttributes)
+{ 
+	this->modifySelfAttributes = modifySelfAttributes;
+	this->modifyMonsterAttributes = modifyMonsterAttributes;
 
 }
+
+void Skill::SingleSkill::AddAbility(int target, int attribute, int symbol, int amount)
+{
+    pair<int,int> skillProperty(symbol,amount);
+	if (target)
+        
+		this->modifyMonsterAttributes[attribute] = skillProperty;
+	else
+		this->modifySelfAttributes[attribute] = skillProperty;
+}
+
+
+Skill::Skill(vector<pair<string,SingleSkill>> setOfSkills)
+{
+	for (pair<string,SingleSkill> currentSkill:setOfSkills)
+	{
+		this->AddSkill(currentSkill);
+	}
+}
+
+int Skill::AddSkill(pair<string,SingleSkill> oneSkill)
+{
+	this->skillLibrary.push_back(oneSkill);
+    return this->skillLibrary.size()-1;
+}
+
 
 string Skill::toString() const
 {
--- Skill.h
+++ Skill.h
@@ -1,14 +1,59 @@
 #ifndef SKILL_H_
 #define SKILL_H_
 #include <string>
+#include <utility>
+#include <map>
+#include <vector>
 using std::string;
+using std::map;
+using std::pair;
+using std::vector;
+
+// Skill class is a libray that contains multiple Single Skills
+// To use a skill, try UseSkill("Ex-Calibur") or UseSkill(1)
 
 class Skill
 {
 private:
-    
+	class SingleSkill
+	{
+	private:
+		// first choose wchih attributes(index 0 ~ 3) to modify (use the key value of type int)
+		// then choose which operator(+,*) and the amount of modification
+		// pair(operator, amount)
+		// by using an operator, we can not only change attributes by plus and minus
+		// but can change them by percentage
+
+		// TO DO ...................
+		// index: 0 attack, 1 defense, 2 stamina, 3 intelligence, 4 stun, 
+		// operator: 0 for +, 1 for *, -1 for stay the same
+
+
+		vector<pair<int,int>> modifySelfAttributes;
+		vector<pair<int,int>> modifyMonsterAttributes;
+		//vector<int> actions
+	public:
+		SingleSkill(vector<pair<int,int>> modifySelfAttributes, vector<pair<int,int>> modifyMonsterAttributes);
+		
+		// target: 0 for self, 1 for monster
+		void AddAbility(int target, int attribute, int symbol, int amount);
+	};
+	// give skill number(array index) to specific skill -- a pair of skill name and skill property
+	vector<pair<string,SingleSkill>> skillLibrary;
+
 public:
-    Skill();
+    Skill(){}
+    Skill(vector<pair<string,SingleSkill>> setOfSkills);
+
+    // returns skill number(mapped value) for the new skill
+    int AddSkill(pair<string,SingleSkill> oneSkill);
+
+    bool UseSkill(string skillName);
+    bool UseSkill(int skillNumber);
+    
+    // print out: 1. skill number
+   	// 			  2. skill name
+   	// 			  3. skill property
     string toString() const;
 };
 #endif
